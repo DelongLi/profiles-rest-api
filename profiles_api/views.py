24b55baeaefec73231acfb615ea6e20e60cc8cd0
Delong Li <lidelong0315@gmail.com>
2023-02-24 11:34:07 -0800

    added search feature to userprofile API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -11,6 +11,7 @@ from profiles_api import models
 from profiles_api import permissions
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 
 class HelloViewSet(viewsets.ViewSet):
@@ -114,3 +115,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
